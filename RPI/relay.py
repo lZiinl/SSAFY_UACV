7abd6ec5491b602fdf0aaf81be1d9f81c9ec1436
조정훈 <hoon970505@naver.com>
2024-07-23 10:29:08 +0900

    feat : relay(solenoid_actuator) code

---
+++ RPI/relay.py
@@ -0,0 +1,25 @@
+from gpiozero import OutputDevice
+from time import sleep
+
+# GPIO 핀 번호 설정
+relay = OutputDevice(17, active_high=True, initial_value=False)  # 릴레이가 HIGH로 활성화됨
+
+# 릴레이 제어 함수
+def relay_on():
+    relay.on()  # 릴레이 켜기
+    print("Relay is ON")
+
+def relay_off():
+    relay.off()  # 릴레이 끄기
+    print("Relay is OFF")
+
+try:
+    while True:
+        relay_on()  # 릴레이 켜기
+        sleep(5)  # 5초 대기
+        relay_off()  # 릴레이 끄기
+        sleep(5)  # 5초 대기
+
+except KeyboardInterrupt:
+    print("Program terminated")
+
